cf8386e158a9ba39b221fd81260ba0316660027e
rim0523rim <hr3250c@gmail.com>
2023-08-30 23:21:39 +0900

    반복후빠져나가기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -1,19 +1,25 @@
 package com.sbs.exam.board;
-
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("==게시판 v 0.1");
         System.out.println("==프로그램 시작 ==");
-        System.out.printf("명령");
-        String cmd = scanner.nextLine();
-        System.out.printf("입력된명령어 : %s\n", cmd);
-        System.out.println("==프로그램 종료==");
 
-        scanner.close();
+        while (true) {
+            System.out.printf("명령");
+            String cmd = scanner.nextLine();
+            System.out.printf("입력된명령어 : %s\n", cmd);
+
+        if (cmd.equals("exit")) {
+            break;
+        }
     }
+            System.out.println("==프로그램 종료==");
+            scanner.close();
+
+    }
+
 }
