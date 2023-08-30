c8d4041789ae0f4892754962972859c3b409a3b7
rim0523rim <hr3250c@gmail.com>
2023-08-30 22:57:52 +0900

    입력후종료

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -1,10 +1,19 @@
 package com.sbs.exam.board;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-    System.out.println("==게시판 v 0.1");
-    System.out.println("==프로그램 시작 ==");
-    System.out.println("명령)");
-    System.out.println("==프로그램 종료==");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("==게시판 v 0.1");
+        System.out.println("==프로그램 시작 ==");
+        System.out.printf("명령");
+        String cmd = scanner.nextLine();
+        System.out.printf("입력된명령어 : %s\n", cmd);
+        System.out.println("==프로그램 종료==");
+
+        scanner.close();
     }
 }
